0ded7289098bd131dc4981857e0ac74d45ef9144
Caesar Chi <caesar_chi@miiicasa.com>
2012-04-19 15:28:45 +0800

    fixed message, and show path timing.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs'),
+    spawn = require('child_process').spawn,
     ws = require('websock'),
     socket,
     config = {
@@ -12,6 +13,7 @@ var fs = require('fs'),
         fileType : [
             '*.php',
             '*.js',
+            '*.scss',
             '*.css',
             '*.php',
             '*.html',
@@ -19,15 +21,18 @@ var fs = require('fs'),
         ]
     },
     cli = require('./cli')(config.fileType),
-    HOMEPATH = process.env.PWD,
-    files= [];
+    HOMEPATH = process.env.PWD;
 
-process.setMaxListeners(0);
+config.port = process.argv[2] || config.port;
+config.port = parseInt(config.port, 10);
 
 cli.getFileList('data', function (files) {
-    this.files = files;
-    this.files.forEach(function (file) {
+    files.forEach(function (file) {
         fs.watchFile(file, function (curr, prev) {
+            if (file.indexOf(".scss") > -1) {
+                spawn('compass_lite', [file, file.replace(".scss", ".css")]);
+                return;
+            }
             if (config.ready) {
                 onChange(file, curr, prev);
             }
@@ -36,23 +41,27 @@ cli.getFileList('data', function (files) {
 });
 cli.run();
 
-ws.listen(config.port, function (socket) {
-    config.socket = socket;
-    console.log( 'Browser: Connected socket (' + socket.version + ')' );
+var connect = function (port) {
+    console.log("connect server...");
+    ws.listen(config.port, function (socket) {
+        config.socket = socket;
+        console.log( 'Browser: Connected socket (' + socket.version + ')' );
 
-    socket.send( "!!ver:" + config.LiveReload );
+        socket.send( "!!ver:" + config.LiveReload );
 
-    socket.on('message', function ( msg ) {
-      console.log( 'Browser: ' + msg );
+        socket.on('message', function ( msg ) {
+          console.log( 'Browser: ' + msg );
+        });
+        socket.on('close', function () {
+          console.log( 'Browser: Disconnected');
+        });
+        config.ready = true;
     });
-    socket.on('close', function () {
-      console.log( 'Browser: Disconnected');
-    });
-    config.ready = true;
-});
+};
 
 var onChange = function (path, current, previous) {
   if (current.mtime > previous.mtime) {
+      console.log("Changed : " + path);
       sendRefresh(HOMEPATH);
   };
 };
@@ -64,9 +73,12 @@ var sendRefresh = function (path) {
     apply_css_live: config.applyCSSLive,
     apply_images_live: config.applyIMGLive
   }]);
-  console.log( "Sending 'Refresh' command.." );
 
   config.socket.send(message);
 };
 
-console.log( 'Simple-livereload is starting...' );
+console.log('Simple-livereload is starting...');
+console.log('server port :' + config.port);
+
+//initial websocket server
+connect(config.port);
