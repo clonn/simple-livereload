9658b803c3c84db16700dd32dfb3c79b18ced171
Caesar Chi <clonncd@gmail.com>
2012-07-04 23:49:11 +0800

    set config.js, load config object from external files.

---
+++ config.js
@@ -0,0 +1,27 @@
+/**
+ * @overview
+ *
+ * @author 
+ * @version 2012/07/04
+ */
+
+module.exports = {
+    ready: false,
+    LiveReload: '1.6',
+    applyJSLive: false,
+    applyCSSLive: true,
+    applyIMGLive: true,
+    port: 35728,
+    includeFile: [
+        'php',
+        'css',
+        'scss',
+        'sass',
+        'js'
+    ],
+    excludeFile: [
+        'swp',
+        'swa'
+    ]
+};
+
