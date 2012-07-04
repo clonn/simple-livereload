8a73e307452419e5ef36b64cfc163c1f26e40fc1
Caesar Chi <clonncd@gmail.com>
2012-07-04 23:48:15 +0800

    main.js , now it works fine and can watch file corss OS

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
 var fs = require('fs');
+var config = require('./config');
+var configFolderPath = process.cwd();
+var vm = require('vm');
 var path = require('path');
-var _watchedFiles = [];
 
-//module.exports.reverseFile = reverseFile function (dirname) {
 function reverseFile (dirname) {
-    var dirname = dirname || __dirname;
+    var dirname = dirname || configFolderPath;
     fs.readdir(dirname, function (err, files) {
 
         files.forEach(function (file, idx) {
-            var fullpath = path.join(dirname, file);
+            var fullpath = path.join(dirname, file),
+                executeFlag = true;
+
             fs.stat(fullpath, function (err, stats) {
 
                 if (stats.isDirectory()) {
                     reverseFile(fullpath);
                 }
                 else {
-                    if (fullpath.match('.swp')) {
+                    config.excludeFile.forEach(function (fp, idx) {
+                        if (fullpath.match(fp)) {
+                            executeFlag = false;
+                            return;
+                        }
+                    });
+
+                    if (!executeFlag) {
                         return;
                     }
-                    watchFile(fullpath);
+
+                    config.includeFile.forEach(function (fp, idx) {
+                        if (fullpath.match(fp)) {
+                            watchFile(fullpath);
+                            return;
+                        }
+                    });
                 }
 
             });
@@ -28,28 +44,64 @@ function reverseFile (dirname) {
 
 function watchFile(fullpath) {
 
-    var o = fs.watchFile(fullpath, {presistent: true}, function (type, filename) {
+    fs.watchFile(fullpath, {presistent: true}, function (curr, prev) {
 
-            /*
-        switch (type) {
-            case 'rename':
-                fs.unwatchFile(fullpath);
-                //watchFile(fullpath);
-                break;
-            default:
-                console.dir(type);
-                console.dir(filename);
-                break;
-        }
-        */
-    });
-    o.on('change', function (curr, prev) {
         if (+curr.mtime !== +prev.mtime) {
             console.log(fullpath + ': File modified');
+            module.exports.fire('update', {
+                fullpath: fullpath,
+                curr: curr,
+                prev: prev
+            });
 
         }
     });
 
 }
 
-reverseFile(__dirname);
+/**
+  * Set export instances, and set setter.
+  */ 
+
+module.exports.callbacks = []; 
+
+module.exports.on = function (event, cb) {
+    if (this.callbacks[event]) {
+        console.log('this event is register');
+        return;
+    }
+
+    this.callbacks[event] = cb;
+}
+
+module.exports.fire = function (event, obj) {
+    if (this.callbacks[event]) {
+        this.callbacks[event].call(this, obj);
+    }
+};
+
+module.exports.setConfig = function (path) { 
+    
+        configFolderPath = path || configFolderPath;        
+        console.log('Setup => Loading Path :' + configFolderPath);
+
+        var configFile = 'config.js',
+            encode = 'utf8',
+            src; 
+
+        try {
+            src = fs.readFileSync(configFolderPath + '/' + configFile, encode);
+            vm.runInNewContext(config, src, configFile);
+        } catch (e) {
+        
+        }
+
+};
+
+module.exports.run = function () {
+    console.log('Start Watching path is :' + configFolderPath);
+    reverseFile(configFolderPath);
+};
+
+module.exports.config = config;
+module.exports.folderPath = configFolderPath;
